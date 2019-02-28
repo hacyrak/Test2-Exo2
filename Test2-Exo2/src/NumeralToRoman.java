5397547a5abc8a8cbfd77e63d9c17fff267cc61f
a1007744 <a1007744@a-f004249.salsa.univ-amu.fr>
2019-02-28 16:36:10 +0100

    Exo2

---
+++ Test2-Exo2/src/NumeralToRoman.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class NumeralToRoman {
+	protected HashMap<Integer,String> Ci = new HashMap<Integer, String>();
+	public NumeralToRoman(){
+		this.Ci.put(1,"I");
+		this.Ci.put(5,"V");
+		this.Ci.put(10,"X");
+		this.Ci.put(50,"L");
+		this.Ci.put(100,"C");
+		this.Ci.put(500,"D");
+		this.Ci.put(1000,"M");
+	}
+	
+	public String getRoman(int nombre) {
+		String res ="";
+		
+		
+		
+		
+		return res;
+	}
+}
